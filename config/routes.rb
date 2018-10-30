c04c6f974771542ab7821b764a91760c8fe17afc
Lili Molloy <liliana.molloy@gmail.com>
2018-10-30 22:56:24 +1100

    completed the controller-scaffold for user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   #user profile routes
-  # get 'user_profile', to: 'user_profile#new' I dont think I will need a new page because the user profile will automatically be created when user signs up. They then can go to the edit page to add additional info
-
-  get 'user_profile/:id', to: 'user_profile#show'
-  get 'user_profile/:id/edit', to: 'user_profile#edit'
-  post 'user_profile/:id', to: 'user_profile#update'
+  resources :user_profiles
 
   get 'show/edit'
   get 'show/new'
