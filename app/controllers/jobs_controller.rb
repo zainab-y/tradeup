8076ea4e4de67f70e8022574e515d522d3804715
Aitzu Hu <aitzuhu@Aitzude-MacBook-Pro.local>
2018-10-31 12:10:10 +1100

    created the job new form

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -72,6 +72,6 @@ class JobsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def job_params
-      params.require(:job).permit(:title, :description, :tenant_available_time, :image)
+      params.require(:job).permit(:title, :description, :tenant_available_time, :image, :job_category)
     end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,4 @@
 class Job < ApplicationRecord
     has_one_attached :image   
+    belongs_to :job_category
 end
