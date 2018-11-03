6d9490dce56bac6607e2cf74f18760b77ea8c0a7
Lili Molloy <liliana.molloy@gmail.com>
2018-11-03 21:06:08 +1100

    added 400 error page and 500 error page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
+
+  root 'pages#welcome'
+
   #user profile routes
   resources :user_profiles
   get 'user_profiles/:id/my_jobs', to: 'user_profiles#my_jobs', as: 'my_jobs'
@@ -13,8 +20,6 @@ Rails.application.routes.draw do
   get 'jobs/:id/accept', to: 'jobs#accept', as: 'jobs_accept'
   get 'jobs/:id/complete', to: 'jobs#complete', as: 'jobs_complete'
   get 'jobs/:id/pay', to: 'jobs#pay', as: 'jobs_pay'
-
-  root 'pages#welcome'
 
   resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
